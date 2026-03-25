7dae9edaf766f89a64e92a5cc22b4b9b325415ab
Manish Jaiswal <manish.jaiswal@finaccel.co>
2026-03-26 00:31:21 +0530

    Implement weather station using observer pattern

---
+++ src/Observer/WeatherPlatform/WeatherStation.java
@@ -0,0 +1,17 @@
+package Observer.WeatherPlatform;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionDisplay currentDisplay =
+                new CurrentConditionDisplay(weatherData);
+
+        // todo: add more display elements
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
+        currentDisplay.display();
+    }
+}
